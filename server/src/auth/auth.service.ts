ba34c42126ae253b29c89ea01b834314b1098bda
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-13 17:49:24 +0300

    added ability to sign tokens

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier */
+import { jwtSecret } from './../utils/constants';
 import { NewUserDTO } from './../dto/newuser.dto';
 import { PrismaService } from './../../prisma/prisma.service';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async signup(data: NewUserDTO) {
     const { email, password, username } = data;
@@ -15,14 +17,29 @@ export class AuthService {
     // Check if user exists.
     const user = await this.getUserByEmail(email);
 
-    if(user) {
-      throw new BadRequestException("Email Already Exists")
+    if (user) {
+      throw new BadRequestException('Email Already Exists');
     }
 
     // Hashed Password.
     const hashedPassword = await this.hashPassword(password);
 
-    return hashedPassword;
+    // get access tokens.
+    const token = await this.signTokens({ email, username });
+
+    // new user.
+    const newUser = await this.createNewUser({
+      email,
+      username,
+      password: hashedPassword,
+      accessToken: token,
+    });
+
+    return {
+      user: newUser,
+      message: 'Signed up successfull!',
+      loggedIn: true
+    };
   }
 
   async sigin() {
@@ -47,7 +64,32 @@ export class AuthService {
     return await this.prisma.user.findUnique({
       where: {
         email,
-      }
-    })
+      },
+    });
+  }
+
+  // create new user.
+  async createNewUser(args: {
+    email: string;
+    username: string;
+    password: string;
+    accessToken: string;
+  }): Promise<User> {
+    const { email, username, password, accessToken } = args;
+    return await this.prisma.user.create({
+      data: {
+        email,
+        username,
+        password,
+        accessToken,
+      },
+    });
+  }
+
+  // sign tokens.
+  async signTokens(payload: { email: string; username: string }): Promise<string> {
+    return await this.jwtService.sign(payload, {
+      secret: jwtSecret,
+    });
   }
 }
