83d6756e45f76e5c644e348499cf425d1eea64f3
DevHumbleChris <dev.humblechris254@gmail.com>
2023-03-13 06:10:34 +0300

    added prisma service and module

---
+++ server/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { PrismaService } from './prisma.service';
+import { Module, Global } from '@nestjs/common';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
